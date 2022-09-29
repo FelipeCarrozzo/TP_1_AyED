3770738049e21291e528834301d6f93c6fc70683
JpGonzalez8 <110999294+JpGonzalez8@users.noreply.github.com>
2022-09-29 18:44:48 -0300

    La mezcla Natural anda!

--- Ejercicio3/modulos/MezclaDivisionNatural.py
+++ Ejercicio3/modulos/MezclaDivisionNatural.py
@@ -11,6 +11,7 @@ def dividir_archivo (archivo,F1,F2):
         f1.write(dato1)
         dato2 = datos.readline()
         archi_aux = f1
+        
         while dato2:   #mientras exista
             if dato2<dato1:
                 if archi_aux == f2:
@@ -21,64 +22,81 @@ def dividir_archivo (archivo,F1,F2):
             archi_aux.write(dato2)
             dato1 = dato2
             dato2 = datos.readline()
-        return dato1, dato2
-
+    
+    
 def mezclar_archivos(archivo1,archivo2,archivo_salida):
     with open(archivo1,"r") as f1, open(archivo2,"r") as f2, open(archivo_salida, "w")as f:
         dato1 = f1.readline()        # es el primer dato de f1
         dato2 = f2.readline()        # es el primer dato de f2
         dato_aux = dato1             # es el dato que queremos escribir en la lista de salida 
-          
+        ordenada = True
         sublistaf1 = True
         sublistaf2 = True
-        while dato1 and dato2:    #mientras existan datos en dato1 y dato2 
-            
-            if dato2 < dato1:
-                dato_aux = dato2
-                dato2 = f2.readline()
-                if dato2 < dato_aux:
-                    
-                    sublistaf2 = False 
-            else:
+        if dato2 == '':
+            while dato1:
                 dato_aux = dato1
-                dato1 = f1.readline()       
-                if dato1 < dato_aux:
-                    
-                    sublistaf1 = False
-                    
-            if sublistaf1 == False:
-                f.write(dato_aux)
-                while dato_aux < dato2:
+                dato1 = f1.readline() 
+                f.write(dato_aux) 
+            return ordenada
+        else:    
+            while dato1 and dato2:    #mientras existan datos en dato1 y dato2 
+                
+                if dato2 < dato1:
                     dato_aux = dato2
                     dato2 = f2.readline()
-                    f.write(dato_aux)
-                
-                
-            elif sublistaf2 == False:
-                f.write(dato_aux)
-                while dato_aux < dato1:
+                    if dato2 < dato_aux:
+                        
+                        sublistaf2 = False 
+                else:
                     dato_aux = dato1
-                    dato1 = f1.readline()
+                    dato1 = f1.readline()       
+                    if dato1 < dato_aux:
+                        
+                        sublistaf1 = False
+                        
+                if sublistaf1 == False:
                     f.write(dato_aux)
+                    while dato_aux < dato2:
+                        dato_aux = dato2
+                        dato2 = f2.readline()
+                        f.write(dato_aux)
+                    
+                    
+                elif sublistaf2 == False:
+                    f.write(dato_aux)
+                    while dato_aux < dato1:
+                        dato_aux = dato1
+                        dato1 = f1.readline()
+                        f.write(dato_aux)
+                    
+                if sublistaf1 != False and sublistaf2 != False:
+                    f.write(dato_aux)
+                else:
+                    sublistaf1 = True    
+                    sublistaf2 = True
                 
-            if sublistaf1 != False and sublistaf2 != False:
+            while dato1 == '' and dato2 != '':  
+                dato_aux = dato2
+                dato2 = f2.readline()
                 f.write(dato_aux)
-            else:
-                sublistaf1 = True    
-                sublistaf2 = True
+                
+            while dato2 == '' and dato1 != '':
+                dato_aux = dato1
+                dato1 = f1.readline()
+                f.write(dato_aux)
             
-        while dato2 and dato1 == None:  #No anda este while
-            dato_aux = dato2
-            dato2 = f2.readline()
-            f.write(dato_aux)
-        while dato1 and dato2 == None:
-            dato_aux = dato1
-            dato1 = f1.readline()
-            f.write(dato_aux)
-        
         
 if __name__ == '__main__':
-    datos = dividir_archivo("./datos.txt","./f1.txt","./f2.txt")
-    print(datos)
+    
+    with open("datos.txt") as datos, open("f1.txt","w") as f1, open("f2.txt","w")as f2:
+    
+        data = 0
+        while data != "ordenda":
+        
+            datos = dividir_archivo("./datos.txt","./f1.txt","./f2.txt")
+    
+            data = mezclar_archivos("./f1.txt", "./f2.txt", "./datos.txt")
+            if data == True:
+                break
     
     
